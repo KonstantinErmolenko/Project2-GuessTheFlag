752f3434217ab093269cd1d9b5f3a51fc7e3e82f
Konstantin Ermolenko <ermolenko.konst@yandex.ru>
2020-03-15 17:07:39 +0300

    Initial Commit

---
+++ Project2-GuessTheFlag/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project2-GuessTheFlag
+//
+//  Created by Ермоленко Константин on 15.03.2020.
+//  Copyright © 2020 Ермоленко Константин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
